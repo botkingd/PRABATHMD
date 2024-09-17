ebf5ec9c57871c0c92fe917629692e7b7d74476a
botkingd <119658836+botkingd@users.noreply.github.com>
2024-09-17 19:22:39 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94703170368"
+const SESSION_ID = "PRABATH-MD~fSRVjThY#PMlpzJsKTeODhTdmKSe2kAgRp8G_iQZoyXdlHgpYGZg"
 
 
 module.exports = {
